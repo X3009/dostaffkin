d77dd655e567dc8981655ad70e813f2d327c1a41
VtlBz <v@vtlbz.com>
2026-05-18 21:12:55 +0500

    add loader to track page

--- src/app/pages/track/track.ts
+++ src/app/pages/track/track.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { finalize } from 'rxjs';
 import { Header } from '../../header/header';
 import { DeliveryApi } from '../../services/delivery-api';
 
@@ -12,6 +13,9 @@ import { DeliveryApi } from '../../services/delivery-api';
 export class Track {
   trackNumber = '';
   trackResult: any = signal(null);
+  trackLoading = signal(false);
+  trackResultVisible = signal(false);
+  private lastSuccessfulTrackNumber: number | null = null;
 
   constructor(private deliveryApi: DeliveryApi) {}
 
@@ -20,23 +24,44 @@ export class Track {
 
     if (!rawValue) {
       alert('Заполните номер отправления');
+      this.trackResultVisible.set(false);
       return;
     }
 
-    this.trackResult.set(null);
     const numericValue = Number(rawValue);
     if (Number.isNaN(numericValue) || numericValue <= 0) {
       alert('Введите корректный номер отправления');
+      this.trackResultVisible.set(false);
       return;
     }
 
-    this.deliveryApi.getDeliveryInfo(numericValue).subscribe((response) => {
-      if ('error' in response) {
-        alert(response.error);
-        return;
-      }
+    const isSameAsLastSuccessful = this.lastSuccessfulTrackNumber === numericValue;
 
-      this.trackResult.set(response);
-    });
+    this.trackResultVisible.set(true);
+    if (!isSameAsLastSuccessful) {
+      this.trackResult.set(null);
+    }
+    this.trackLoading.set(true);
+
+    this.deliveryApi
+      .getDeliveryInfo(numericValue)
+      .pipe(finalize(() => this.trackLoading.set(false)))
+      .subscribe((response) => {
+        if ('error' in response) {
+          alert(response.error);
+          const hasPreviousResult = this.trackResult() !== null;
+          if (isSameAsLastSuccessful && hasPreviousResult) {
+            this.trackResultVisible.set(true);
+            return;
+          }
+
+          this.trackResult.set(null);
+          this.trackResultVisible.set(false);
+          return;
+        }
+
+        this.trackResult.set(response);
+        this.lastSuccessfulTrackNumber = numericValue;
+      });
   }
 }
